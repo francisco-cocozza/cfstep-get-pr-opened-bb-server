35f70eea121ab9f85dc49b03d64b793437004783
bb-admin <support@codefresh.io>
2019-05-01 07:13:22 -0600

    Adding cf_export for PR_ID var

--- script/run.sh
+++ script/run.sh
@@ -3,8 +3,6 @@
 ##   Retrieve a page of pull requests in the current repository that contain the given commit.   
 ##  Details: https://docs.atlassian.com/bitbucket-server/rest/6.1.2/bitbucket-rest.html#idp206
 echo "Get PRs ==> /rest/api/1.0/projects/${PROJECT_KEY}/repos/${REPOSITORY}/commits/${COMMIT_ID}/pull-requests"
-echo "command to execute"
-echo "curl -s -u ${USERNAME}:${PASSWORD} ${BB_URL}/rest/api/1.0/projects/${PROJECT_KEY}/repos/${REPOSITORY}/commits/${COMMIT_ID}/pull-requests"
 PRS_OF_COMMIT=$(curl -s -u ${USERNAME}:${PASSWORD} ${BB_URL}/rest/api/1.0/projects/${PROJECT_KEY}/repos/${REPOSITORY}/commits/${COMMIT_ID}/pull-requests)
 
 ##  If there are PRs associated to that COMMIT_ID, then, filter the PRs by those that match the same BRANCH name and with state OPEN
@@ -19,4 +17,7 @@ echo "PR_ID is '${PR_ID}'"
 
 ## Export the var locally and at CF level
 export PR_ID=${PR_ID}
-# cf_export PR_ID
+if [ "$CF_URL" != "" ]; then
+    cf_export PR_ID
+fi
+
