80c26e40e5d7df84aac736ddb120ad13badc48bc
bb-admin <support@codefresh.io>
2019-05-01 07:18:05 -0600

    Fixing echo for PR_ID

--- script/run.sh
+++ script/run.sh
@@ -9,11 +9,10 @@ PRS_OF_COMMIT=$(curl -s -u ${USERNAME}:${PASSWORD} ${BB_URL}/rest/api/1.0/projec
 if [ $(echo ${PRS_OF_COMMIT} | jq '.values | length') -ge 1 ]; then
     echo "Found PR(s) for that commit"
     PR_ID=$(echo ${PRS_OF_COMMIT} | jq --arg BRANCH "refs/heads/$BRANCH" '.values[] | select( ( .fromRef.id == $BRANCH ) and ( .state == "OPEN" ) )' | jq .id )
+    echo "PR_ID for that commit and branch is: '${PR_ID}'"
 else
     echo "No Prs found for that commit"
 fi
-
-echo "PR_ID is '${PR_ID}'"
 
 ## Export the var locally and at CF level
 export PR_ID=${PR_ID}
