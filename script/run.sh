e60874bc12f263474b189ba6af25cb368a164bb2
bb-admin <support@codefresh.io>
2019-05-01 08:51:27 -0600

    Adding -k option to curl To allow self-certificates in BB-server

--- script/run.sh
+++ script/run.sh
@@ -3,7 +3,7 @@
 ##   Retrieve a page of pull requests in the current repository that contain the given commit.   
 ##  Details: https://docs.atlassian.com/bitbucket-server/rest/6.1.2/bitbucket-rest.html#idp206
 echo "Get PRs ==> /rest/api/1.0/projects/${PROJECT_KEY}/repos/${REPOSITORY}/commits/${COMMIT_ID}/pull-requests"
-PRS_OF_COMMIT=$(curl -s -u ${USERNAME}:${PASSWORD} ${BB_URL}/rest/api/1.0/projects/${PROJECT_KEY}/repos/${REPOSITORY}/commits/${COMMIT_ID}/pull-requests)
+PRS_OF_COMMIT=$(curl -k -s -u ${USERNAME}:${PASSWORD} ${BB_URL}/rest/api/1.0/projects/${PROJECT_KEY}/repos/${REPOSITORY}/commits/${COMMIT_ID}/pull-requests)
 
 ##  If there are PRs associated to that COMMIT_ID, then, filter the PRs by those that match the same BRANCH name and with state OPEN
 if [ $(echo ${PRS_OF_COMMIT} | jq '.values | length') -ge 1 ]; then
